83193464e0dc71b15fe6bbed18d027befec7d493
JM <jmagali.dev@gmail.com>
2025-07-23 00:04:37 -0400

    Create divide function

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function subtract(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Error! Cannot divide by 0!";
+    }
+
+    return a / b;
+}
